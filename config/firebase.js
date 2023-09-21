8019513340c50b1e745b949e6d3a828ecde73fd6
Timothy Austin <timothyonyea@gmail.com>
2023-09-21 12:43:33 +0100

    error fix

--- config/firebase.js
+++ config/firebase.js
@@ -11,8 +11,7 @@ import { getFirestore, serverTimestamp } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_DEV_API_KEY,
-  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,  
   projectId: process.env.REACT_APP_DEV_PROJECT_ID,
   storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
