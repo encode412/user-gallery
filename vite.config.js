a5e602cf6b5b83ffabda38b23aa75fe4654d3838
Timothy Austin <timothyonyea@gmail.com>
2023-09-21 11:36:12 +0100

    edit error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { build } from 'vite';
+// import { build } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -20,7 +20,11 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react()
     ],
-    base: '/',
+    root: './',  
+    publicDir: 'assets',    
+    build: {
+      outDir: 'dist',
+    }
   }
   // ${process.env.BASE_URL}
 })
